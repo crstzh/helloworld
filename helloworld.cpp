d9923262f67127a247ab310dc23381d66285c821
zhanghui <crst_zh@126.com>
2023-12-20 11:00:55 +0800

    inlucde stdio.h

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-
+#include "stdio.h"
 int add(int x, int y)
 {
-    return x+y;
+   return x + y;
 }
 
 
 int main()
 {
-  int sum = add(2,3);
-  printf("sum=%d\n", sum);
-  std::cout << "Hello World!\n";  
+   int sum = add(2, 3);
+   printf("sum=%d\n", sum);
+   printf("Hello World!\n");
 }
